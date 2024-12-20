66e85355f7444480655b09ee267ebb0ac0361e87
Amir Abdeldjalil <zruchiha005@gmail.com>
2024-12-20 12:10:49 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MorePractise
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
